7a17059100177a9e7ed32a5fd28a26f1c5185b26
inwoo-machine <inwoo@snu.ac.kr>
2024-04-18 15:41:31 +0900

    CHG: fix build SEMIC-cpp with pybind11.
    	수정함:        CMakeLists.txt
    	이름 바꿈:     src/pybind.cpp -> src/pySEMIC.cpp

--- src/pySEMIC.cpp
+++ src/pySEMIC.cpp
@@ -3,10 +3,11 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(semic, m){
+PYBIND11_MODULE(pySEMIC, m){
 	m.doc() = "SEMIC module in python.";
 
 	py::class_<SEMIC>(m, "SEMIC")
-		.def(py::init<>())
-		.def("Initialize",&SEMIC::Initialize);
+	   .def(py::init<>())
+		.def("Initialize",&SEMIC::Initialize)
+		.def("Display",&SEMIC::Display);
 }
