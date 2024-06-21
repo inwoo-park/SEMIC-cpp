0897809e3e7392b359c4347a04956fa3a16af2f1
inwoo-simba <inwoo@snu.ac.kr>
2024-06-21 23:29:26 +0900

    update install-SEMIC-cpp.sh

--- install-SEMIC-cpp.sh
+++ install-SEMIC-cpp.sh
@@ -5,4 +5,5 @@ cwd=$(pwd)
 
 rm -rf build
 mkdir build
-cd build && cmake ../ -L -DCMAKE_INSTALL_PREFIX=${cwd}/install && make && make install
+cd build && cmake ../ -L -DCMAKE_INSTALL_PREFIX=${cwd}/install -DUSE_OPENMP=OFF
+make && make install
