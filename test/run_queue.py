b69e7fbe6e69f97da2f9771ea28e4c2f08feb456
inwoo-machine <inwoo@snu.ac.kr>
2024-07-04 10:07:52 +0900

    CHG: run_queue.py - update.

--- test/run_queue.py
+++ test/run_queue.py
@@ -6,11 +6,18 @@ import os, sys, socket
 hostname = socket.gethostname().lower().replace('-','')
 
 # Okay, generate qscript
-ngen  = 50
-#npop  = 100
-#omega = 0.8
+pso_method = 'APSO' # or 'wPSO'
+ngen  = 100 
+#freq = 'day'
+freq = 'mon'
+if freq == 'day':
+    amp_max = 5.
+elif freq == 'mon':
+    amp_max = 10.
+#alb_scheme = 'denby'
+alb_scheme = 'isba'
 
-def script_default(prefix, ngen, npop, omega): # {{{
+def script_default(prefix, ngen, npop, freq, omega, amp_max, alb_scheme='denby', pso_method='wPSO'): # {{{
     script  = ''
     script += '#!/bin/bash\n'
     script += '''\
@@ -35,18 +42,29 @@ cd $HOME/SEMIC-cpp/test/
 
 # okay, run script
 python3 run_OptimizationERA5.py -ngen {ngen} -npop {npop} -debug 0 -omega {omega} -ncpu=30 \
+        -tqdm=0 \
+        -freq={freq} -alb_scheme {alb_scheme} -amp_max={amp_max}\
         -opt_method=2 -datadir "data/{prefix}"
-'''.format(ngen=ngen, npop=npop, omega=omega, prefix=prefix)
+'''.format(ngen=ngen, npop=npop, omega=omega, amp_max=amp_max, freq=freq, prefix=prefix, alb_scheme=alb_scheme)
 
     return script
     # }}}
 
-for npop in [100, 200]:
-    for omega in [0.6, 0.8]:
-        prefix = f'PSO_mon2_omega{omega}_npop{npop}' # initialize prefix of script
+# check consistency {{{
+if not freq in ['day','mon']:
+    raise Exception('ERROR! with freq')
+# }}}
+
+# prepare the list of optimization.
+for npop in [100, 200]: # 100, 200
+    for omega in [0.7, 0.8]: # 0.6, 0.8
+        if freq == 'mon':
+            prefix = f'PSO_{freq}2_omega{omega}_npop{npop}_Alb{alb_scheme}_ampMax{amp_max:.2f}' # initialize prefix of script
+        elif freq == 'day':
+            prefix = f'PSO_{freq}_omega{omega}_npop{npop}_Alb{alb_scheme}_ampMax{amp_max:.2f}' # initialize prefix of script
 
         # get script
-        script = script_default(prefix, ngen, npop, omega)
+        script = script_default(prefix, ngen, npop, freq, omega, amp_max)
 
         # okay, now write script
         with open(f'./run_{prefix}.sh','w') as fid:
@@ -54,4 +72,3 @@ for npop in [100, 200]:
 
         # launch queue script.
         os.system(f'qsub ./run_{prefix}.sh')
-
