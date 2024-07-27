fadd53b90c8c07272ab2a8dfafb51c23067521c5
inwoo-zenbook <inwoopark0415@gmail.com>
2024-07-27 16:04:52 +0900

    add script/install-win.py - just build python library.

---
+++ script/install-win.py
@@ -0,0 +1,11 @@
+#!/bin/bash python3
+
+import os, sys
+
+# change directory for window system.
+cwd = os.path.dirname(__file__)
+os.chdir(cwd)
+
+# Do build script!
+command = 'cd ../ && python setup.py build_ext --inplace'
+os.system(command)
