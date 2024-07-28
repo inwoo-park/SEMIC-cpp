803d8373abf31d05f8ad6554a102ab4f519a15e1
inwoo-zenbook <inwoopark0415@gmail.com>
2024-07-28 20:43:00 +0900

    CHG: pyseb/__init__.py - load "SemicResult" class.

--- pyseb/__init__.py
+++ pyseb/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 from . import io, utils, interp, geometry
-from .libpysemic import SEMIC, Param, Const, DoubleMatrix, SemicForcings
+from .libpysemic import SEMIC, Param, Const, DoubleMatrix, SemicForcings, SemicResult
 from .main import prepare_semic_day
 from . import libexample
