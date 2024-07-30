a301727ea3f3da4704f2405e09ba2260ea204549
inwoo-machine <inwoo@snu.ac.kr>
2024-07-30 17:07:37 +0900

    CHG: test_SemicForcings.py - check memory usage with "memory_profiler."

--- test/test_SemicForcings.py
+++ test/test_SemicForcings.py
@@ -4,14 +4,17 @@ import pyseb
 import numpy as np
 import pytest
 
+from memory_profiler import profile
+
+@profile
 def test_SemicForcings():
     '''Test "SemicForcings" in "pyseb" module
     '''
     f = pyseb.SemicForcings()
 
     # Make random matrix
-    nrow = 10
-    ncol = 2
+    nrow = 365
+    ncol = 1000
     A0 = np.random.random((nrow, ncol))
 
     # Assign A value in t2m
@@ -19,9 +22,13 @@ def test_SemicForcings():
 
     # Get value!
     print(f"t2m size = ({f.t2m.nrow}, {f.t2m.ncol})")
-    print(f.t2m.get_value())
+    #print(f.t2m.get_value())
 
     assert(np.all(f.t2m.get_value() == A0))
+
+    # clear memory
+    del A0
+    del f
 
 if __name__ == '__main__':
     test_SemicForcings()
