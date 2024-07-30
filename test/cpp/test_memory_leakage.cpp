58908430541b7af35c831b7b8f25bece9e4faa5b
inwoo-simba <inwoo@snu.ac.kr>
2024-07-30 20:16:21 +0900

    CHG: test_memory_leakage.cpp - check DoublMatrix memory leakage.

--- test/cpp/test_memory_leakage.cpp
+++ test/cpp/test_memory_leakage.cpp
@@ -1,10 +1,9 @@
 #include "SemicParameters.h"
 #include "SurfaceEnergyBalance.h"
 
-int main(void){
-
-	int nx = 20000;
-	int ntime = 7000;
+void test_class_pointer(){ /* {{{ */
+	int nx = 10000;
+	int ntime = 1000;
 
 	DoubleMatrix *m = new DoubleMatrix(nx, ntime);
 	delete m;
@@ -19,5 +18,32 @@ int main(void){
 	semic->Initialize(nx);
 
 	delete semic;
+} /* }}} */
+
+void test_DoubleMatrix(){ /* {{{ */
+	DoubleMatrix *m = new DoubleMatrix();
+
+	cout << "Initialize m matrix\n";
+	cout << "nx, ny = " << m->nrow << "," << m->ncol << endl;
+
+	vector<vector<double>> a(20, vector<double>(30,0.0));
+	vector<vector<double>> b;
+
+	/* reshape array */
+	m->set_value(a);
+	b = m->get_value();
+
+	cout << "Set value, m matrix\n";
+	cout << "nx, ny = " << m->nrow << "," << m->ncol << endl;
+
+	cout << "Information for b matrix\n";
+	cout << "nx, ny = " << b.size() << "," << b[0].size() << endl;
+
+	delete m;
+} /* }}} */
+
+int main(void){
+	//test_class_pointer();
+	test_DoubleMatrix();
 	return 0;
 }
