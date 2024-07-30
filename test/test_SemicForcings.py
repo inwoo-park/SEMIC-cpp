8662c374ce5b9c59c8feb1a65228da4de66849f3
inwoo-simba <inwoo@snu.ac.kr>
2024-07-30 22:04:28 +0900

    CHG: test/test_SemicForcings.py - fix minor.

--- test/test_SemicForcings.py
+++ test/test_SemicForcings.py
@@ -10,24 +10,29 @@ from memory_profiler import profile
 def test_SemicForcings():
     '''Test "SemicForcings" in "pyseb" module
     '''
-    f = pyseb.SemicForcings()
 
     # Make random matrix
     nrow = 365
     ncol = 1000
     A0 = np.random.random((nrow, ncol))
 
+    f = pyseb.SemicForcings()
+
     # Assign A value in t2m
+    f.sf.set_value(A0)
+    f.rf.set_value(A0)
     f.t2m.set_value(A0)
 
     # Get value!
     print(f"t2m size = ({f.t2m.nrow}, {f.t2m.ncol})")
     #print(f.t2m.get_value())
 
-    assert(np.all(f.t2m.get_value() == A0))
+    k = f.t2m.get_value()
+    assert(np.all(np.array(k) == A0))
 
     # clear memory
     del A0
+    del k
     del f
 
 if __name__ == '__main__':
