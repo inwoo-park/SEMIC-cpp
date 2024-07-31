2a39bf3a8b2c69eccb8ab85bc6c92c1c89a50250
inwoo-machine <inwoo@snu.ac.kr>
2024-07-31 16:57:00 +0900

    CHG: fix installing script.
    	modified:   CMakeLists.txt
    	modified:   install-SEMIC-cpp.sh
    	modified:   script/install-win.py

--- install-SEMIC-cpp.sh
+++ install-SEMIC-cpp.sh
@@ -3,10 +3,13 @@
 # get current director
 cwd=$(pwd)
 
-rm -rf build
-mkdir build
-cd build && cmake ../ -L -DCMAKE_INSTALL_PREFIX=${cwd}/install -DUSE_OPENMP=ON
+if [ ! -d build ]; then
+	mkdir build
+fi
+cd build && cmake ../ -L -DCMAKE_INSTALL_PREFIX=${cwd}/install -DUSE_OPENMP=ON \
+	-Dpybind11_DIR=${PYBIND11_DIR} \
+	-DUSE_DEBUG=OFF
 make && make install
 cd ../
 
-python setup.py build_ext --inplace
+#python setup.py build_ext --inplace
