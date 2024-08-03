470f614f4cfc4603832e144123652fc7d4e81a52
inwoo-machine <inwoo@snu.ac.kr>
2024-08-03 09:27:59 +0900

    CHG: test/test_SemicResults.py - check return dimension from "SemicResult".

--- test/test_SemicResults.py
+++ test/test_SemicResults.py
@@ -5,17 +5,24 @@ import numpy as np
 def test_SemicResult(): # {{{
     nx = 10
     ntime = 20
-    result = pyseb.SemicResult()
+    result = pyseb.SemicResult(nx, ntime)
 
     output_request = result.output_request
     output_list    = result.output_list
 
-    print(f"output list = {output_list}")
+    evap = result.evap.get_value()
+    subl = result.subl.get_value()
 
-    result.evap.get_value()
-    result.subl.get_value()
+    hsnow = result.hsnow.get_value()
+    alb_snow = result.alb_snow.get_value()
+    alb = result.alb.get_value()
 
-    print("Done!")
+    assert(evap.shape == (nx, ntime))
+    assert(subl.shape == (nx, ntime))
+
+    assert(hsnow.shape == (nx, ntime))
+    assert(alb_snow.shape == (nx, ntime))
+    assert(alb.shape == (nx, ntime))
     # }}}
 
 if __name__ == '__main__':
