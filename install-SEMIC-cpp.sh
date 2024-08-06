27b22dec0891bbc45bdcd9f2f2fef5aa113dbb5f
inwoo-simba <inwoo@snu.ac.kr>
2024-08-06 17:13:24 +0900

    CHG: install-SEMIC-cpp.sh - fix script!

--- install-SEMIC-cpp.sh
+++ install-SEMIC-cpp.sh
@@ -6,10 +6,12 @@ cwd=$(pwd)
 if [ ! -d build ]; then
 	mkdir build
 fi
-cd build && cmake ../ -L -DCMAKE_INSTALL_PREFIX=${cwd}/install -DUSE_OPENMP=ON \
-	-Dpybind11_DIR=${PYBIND11_DIR} \
+cd build && cmake ../ \
+	--fresh \
+	-L -DCMAKE_INSTALL_PREFIX=${cwd}/install -DUSE_OPENMP=ON \
 	-DUSE_DEBUG=OFF
 make && make install
 cd ../
 
+#-Dpybind11_DIR=${PYBIND11_DIR} \
 #python setup.py build_ext --inplace
