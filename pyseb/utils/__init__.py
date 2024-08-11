cd9b9db6a460a92eb0354c4adb8de4370656a98e
inwoo-simba <inwoo@snu.ac.kr>
2024-08-12 03:00:26 +0900

    NEW: pyseb/utils/__init__.py > "is_djf" function for finding DJF time.

--- pyseb/utils/__init__.py
+++ pyseb/utils/__init__.py
@@ -7,3 +7,18 @@ from .isnotebook import *
 from .iswindow import iswindow
 from .gethostname import gethostname
 from .cftime2datetime import cftime2datetime
+
+def is_djf(month):
+    '''Find specific month gith given month
+
+    Example
+    -------
+    code-block::python
+        
+        era5_djf = era5.isel(time=pyseb.utils.is_djf(era5['time.year']))
+
+    Returns
+    -------
+    output: bool in np.ndarray
+    '''
+    return (month == 12) | (month == 1) | (month == 2)
