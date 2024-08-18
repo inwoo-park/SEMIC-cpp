5bd110615a2d8cd9d9adea66f313c52909021366
inwoo-zenbook <inwoopark0415@gmail.com>
2024-08-18 17:20:22 +0900

    Update pyseb/utils/__init__.py - check argument parsing with boolean...

--- pyseb/utils/__init__.py
+++ pyseb/utils/__init__.py
@@ -22,3 +22,17 @@ def is_djf(month):
     output: bool in np.ndarray
     '''
     return (month == 12) | (month == 1) | (month == 2)
+
+def args_str2bool(default=True, type=None,help=None, dest=None, option_strings=None):
+    '''Return bool depending on input argument...
+    '''
+    v = default
+    import argparse
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
