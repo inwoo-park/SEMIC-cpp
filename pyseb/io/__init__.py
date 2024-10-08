8fbbd8ef709183e51619436bc15f601702c42518
inwoo-machine <inwoo@snu.ac.kr>
2024-10-08 21:23:48 +0900

    NEW: save_netcdf.py - export semic result in netcdf format.
    	modified:   pyseb/io/__init__.py
    	new file:   pyseb/io/save_netcdf.py

--- pyseb/io/__init__.py
+++ pyseb/io/__init__.py
@@ -1,2 +1,3 @@
 from .get_cmip6_modelname import get_cmip6_modelname
 from .load_netcdf import load_netcdf
+from .save_netcdf import save_netcdf
---
+++ pyseb/io/save_netcdf.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import numpy as np
+import os, sys, platform
+import xarray
+import pyseb
+
+__all__ = ['save_netcdf']
+def save_netcdf(semic:pyseb.SEMIC, fname:str, time:list): # {{{
+    '''
+    Explain
+    -------
+    Export results in SEMIC module in netcdf (.nc) format.
+
+
+    Example
+    -------
+    >>> import pyseb
+    >>> pyseb.io.save_netcdf(semic, 'results.nc')
+
+    Parameters
+    ----------
+    semic: pyseb.SEMIC
+        class SEMIC contains results in format....
+
+    fname: str
+        export file name...
+
+    time: list
+        datetime array.
+    '''
+    raise Exception('ERROR: This is under development.')
+
+    nx    = semic.nx # load number of grid.
+    ntime = len(time) # time stamp for semic simulation.
+
+    # Initialize Dataset array.
+    nc = xarray.Dataset(coords={'time':time,'nx':np.arange(nx)})
+
+    # check requested output
+    output_request = semic.output_request
+    for varname in output_request:
+        if varname in 'smb':
+            value = semic.Result.smb.get_value()
+        elif varname in 'melt':
+            value = semic.Result.melt.get_value()
+        elif varname in 'tsurf':
+            value = semic.Result.tsurf.get_value()
+        elif varname in 'alb':
+            value = semic.Result.alb.get_value()
+        elif varname in 'alb_snow':
+            value = semic.Result.alb_snow.get_value()
+        elif varname in 'hsnow':
+            value = semic.Result.hsnow.get_value()
+        elif varname in 'subl':
+            value = semic.Result.subl.get_value()
+        elif varname in 'evap':
+            value = semic.Result.evap.get_value()
+        else:
+            raise Exception('ERROR: Given variable name (=%s) is not supported.'%(varname))
+
+        nc[varname] = xarray.DataArray(value, dims=('nx','ntime'))
+
+    print(f'Save {fname}')
+    nc.to_netcdf(fname)
+    # }}}
